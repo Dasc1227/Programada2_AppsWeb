17ba8e8be65b022f59e4e6abd8fb20f8e8652267
Dasc1227 <39469718+Dasc1227@users.noreply.github.com>
2020-06-15 14:07:37 -0600

    Add files via upload

---
+++ Ejemplos HolaMundo/EjemploSOAP-PHP5/Clientes/Netbeans 8.0 Java/HolaMundoClient/build/generated-sources/jax-ws/ecci_holamundo/SaludeReturn.java
@@ -0,0 +1,64 @@
+
+package ecci_holamundo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="saludeResult" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "saludeResult"
+})
+@XmlRootElement(name = "saludeReturn")
+public class SaludeReturn {
+
+    @XmlElement(required = true)
+    protected String saludeResult;
+
+    /**
+     * Obtiene el valor de la propiedad saludeResult.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSaludeResult() {
+        return saludeResult;
+    }
+
+    /**
+     * Define el valor de la propiedad saludeResult.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSaludeResult(String value) {
+        this.saludeResult = value;
+    }
+
+}
---
+++ Ejemplos HolaMundo/EjemploSOAP-PHP5/Clientes/Netbeans 8.0 Java/HolaMundoClient/build/generated-sources/jax-ws/ecci_holamundo/ServidorEstampillaDeTiempoReturn.java
@@ -0,0 +1,64 @@
+
+package ecci_holamundo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="servidorEstampillaDeTiempoResult" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "servidorEstampillaDeTiempoResult"
+})
+@XmlRootElement(name = "servidorEstampillaDeTiempoReturn")
+public class ServidorEstampillaDeTiempoReturn {
+
+    @XmlElement(required = true)
+    protected String servidorEstampillaDeTiempoResult;
+
+    /**
+     * Obtiene el valor de la propiedad servidorEstampillaDeTiempoResult.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getServidorEstampillaDeTiempoResult() {
+        return servidorEstampillaDeTiempoResult;
+    }
+
+    /**
+     * Define el valor de la propiedad servidorEstampillaDeTiempoResult.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setServidorEstampillaDeTiempoResult(String value) {
+        this.servidorEstampillaDeTiempoResult = value;
+    }
+
+}
